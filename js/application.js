1efd07948d4b5852c554c1fbe3776179dd11cf83
Michael C Hogan <mhogan@alumni.usc.edu>
2013-01-02 23:31:35 -0700

    Basic sessionStorage capability added
    sessionStorage is being used since data should clear from the browser
    and user device once a user's session ends.

--- js/application.js
+++ js/application.js
@@ -17,22 +17,63 @@ $(document).on("pageinit", function(event){
 
 });
 
-function clickCounter()
-{
-if(typeof(Storage)!=="undefined")
-  {
-  if (sessionStorage.clickcount)
-    {
-    sessionStorage.clickcount=Number(sessionStorage.clickcount)+1;
-    }
-  else
-    {
-    sessionStorage.clickcount=1;
-    }
-  document.getElementById("result").innerHTML="You have clicked the button " + sessionStorage.clickcount + " time(s) in this session.";
-  }
-else
-  {
-  document.getElementById("result").innerHTML="Sorry, your browser does not support web storage...";
-  }
+function clickCounter() {
+	
+	sessionStorage.healthRecordJSONtext = JSON.stringify(healthRecord);
+		
+	if(typeof(Storage)!=="undefined")
+	{
+		if (sessionStorage.clickcount)
+		{
+			sessionStorage.clickcount=Number(sessionStorage.clickcount)+1;
+		}
+		else
+		{
+			sessionStorage.clickcount=1;
+		}
+		
+		document.getElementById("result").innerHTML="You have clicked the button " + sessionStorage.clickcount + " time(s) in this session.";
+	}
+	else
+	{
+		document.getElementById("result").innerHTML="Sorry, your browser does not support web storage...";
+	}
+	
+}
+
+var healthRecord = {
+ "userNumber": 11111111,
+ "userName": "John Doe",
+ "birthdate": 11111,
+ "bloodType": "A",
+ "occupation": "Engineer",
+ "vaccines":{
+  "tetanus": "11111",
+  "hepA": "11111",
+  "hepB": "11111",
+  "hepC": 11111,
+  "tb": 11111,
+  "polio": 11111,
+  "vacA": 11111,
+  "vacB": 11111
+},
+ "allergens": "aaaaaaaa",
+ "conditions": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+ "meds": "aaa"
+}
+
+function shareRecord() {
+	
+	if(typeof(Storage)!=="undefined")
+	{
+		if (sessionStorage.healthRecordJSONtext)
+		{
+			document.getElementById("result").innerHTML=sessionStorage.healthRecordJSONtext;
+		}
+		else
+		{
+			document.getElementById("result").innerHTML="Sorry, no record is available";
+		}
+	}
+		
 }
