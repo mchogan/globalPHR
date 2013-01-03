864ba30a905e74bd6544851da65607d0b5c46c59
Michael C Hogan <mhogan@alumni.usc.edu>
2013-01-02 17:34:25 -0700

    Moved globalphr.js to application.js

--- js/application.js
+++ js/application.js
@@ -1,6 +1,16 @@
-/* 
-	jQuery Mobile Boilerplate
-	application.js
+/*
+	Global Personal Health Record
+	
+	Copyright (c) 2012-2013 Michael C. Hogan
+	
+	URL: http://www.globalphr.com/
+	
+	License: Coming soon. For now, all rights reserved.
+	
+	// The word "QR Code" is registered trademark of 
+	// DENSO WAVE INCORPORATED
+	// http://www.denso-wave.com/qrcode/faqpatent-e.html
+	
 */
 $(document).on("pageinit", function(event){
 	// custom code goes here
