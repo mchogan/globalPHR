f865cea3de38f321933d2d44c4f39bb00904c2e4
Michael C Hogan <mhogan@alumni.usc.edu>
2013-01-03 15:41:16 -0700

    Changed tabs to double spaces

--- js/application.js
+++ js/application.js
@@ -1,75 +1,69 @@
 /*
-	Global Personal Health Record
-	
-	Copyright (c) 2012-2013 Michael C. Hogan
-	
-	URL: http://www.globalphr.com/
-	
-	License: Coming soon. For now, all rights reserved.
-	
-	// The word "QR Code" is registered trademark of 
-	// DENSO WAVE INCORPORATED
-	// http://www.denso-wave.com/qrcode/faqpatent-e.html
-	
+  Global Personal Health Record
+  
+  Copyright (c) 2012-2013 Michael C. Hogan
+  
+  URL: http://www.globalphr.com/
+  
+  License: Coming soon. For now, all rights reserved.
+  
+  // The word "QR Code" is registered trademark of 
+  // DENSO WAVE INCORPORATED
+  // http://www.denso-wave.com/qrcode/faqpatent-e.html
+  
 */
 
 // Initialize application
-$(document).on("pageinit", function(event){
-	// custom code goes here
-	
-	// Disable inputs until user chooses to edit page
-	//$("input,textarea,select,button").prop('disabled', true);
-		
+$(document).on("pageinit", function(event) {
+  // custom code goes here
+  // Disable inputs until user chooses to edit page
+  //$("input,textarea,select,button").prop('disabled', true);
 });
 
 // Initialize #share feature page
-$( document ).delegate("#share", "pageinit", function() {
-	//alert('A page with an id of "share" was just created by jQuery Mobile!');
-	//shareRecord();
+$(document).delegate("#share", "pageinit", function() {
+  //alert('A page with an id of "share" was just created by jQuery Mobile!');
+  //shareRecord();
 });
 
 
-function setUpThisPage(){
-	//Find the last slash character, return the string following the slash
-	var thisFile = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
-	
-	if (thisFile === 'share.html'){
-		shareRecord();
-	}
+function setUpThisPage() {
+  //Find the last slash character, return the string following the slash
+  var thisFile = window.location.href.substr(window.location.href.lastIndexOf("/") + 1);
+
+  if (thisFile === 'share.html') {
+    shareRecord();
+  }
 }
 
 // Get page
-function whatIsThisFile(){	
-	//Find the last slash character, return the string following the slash
-	var filename = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
-	return filename;
+
+function whatIsThisFile() {
+  //Find the last slash character, return the string following the slash
+  var filename = window.location.href.substr(window.location.href.lastIndexOf("/") + 1);
+  return filename;
 }
 
 // ----------------------------------------------------------------------
 /* clickCounter */
 // ----------------------------------------------------------------------
+
 function clickCounter() {
-	
-	sessionStorage.healthRecordJSONtext = JSON.stringify(healthRecord);
-		
-	if(typeof(Storage)!=="undefined")
-	{
-		if (sessionStorage.clickcount)
-		{
-			sessionStorage.clickcount=Number(sessionStorage.clickcount)+1;
-		}
-		else
-		{
-			sessionStorage.clickcount=1;
-		}
-		
-		document.getElementById("result").innerHTML="You have clicked the button " + sessionStorage.clickcount + " time(s) in this session.";
-	}
-	else
-	{
-		document.getElementById("result").innerHTML="Sorry, your browser does not support web storage...";
-	}
-	
+
+  sessionStorage.healthRecordJSONtext = JSON.stringify(healthRecord);
+
+  if (typeof(Storage) !== "undefined") {
+    if (sessionStorage.clickcount) {
+      sessionStorage.clickcount = Number(sessionStorage.clickcount) + 1;
+    } else {
+      sessionStorage.clickcount = 1;
+    }
+
+    document.getElementById("result").innerHTML = "You have clicked the button " + sessionStorage.clickcount + " time(s) in this session.";
+  } else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+  }
+
 }
 
 // ----------------------------------------------------------------------
@@ -79,24 +73,24 @@ function clickCounter() {
 //
 // ----------------------------------------------------------------------
 var healthRecord = {
- "userNumber": 11111111,
- "userName": "John Doe",
- "birthdate": 11111,
- "bloodType": "A",
- "occupation": "Engineer",
- "vaccines":{
-  "tetanus": "11111",
-  "hepA": "11111",
-  "hepB": "11111",
-  "hepC": 11111,
-  "tb": 11111,
-  "polio": 11111,
-  "vacA": 11111,
-  "vacB": 11111
-},
- "allergens": "aaaaaaaa",
- "conditions": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
- "meds": "aaa"
+  "userNumber": 11111111,
+  "userName": "John Doe",
+  "birthdate": 11111,
+  "bloodType": "A",
+  "occupation": "Engineer",
+  "vaccines": {
+    "tetanus": "11111",
+    "hepA": "11111",
+    "hepB": "11111",
+    "hepC": 11111,
+    "tb": 11111,
+    "polio": 11111,
+    "vacA": 11111,
+    "vacB": 11111
+  },
+  "allergens": "aaaaaaaa",
+  "conditions": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+  "meds": "aaa"
 }
 
 // A QR code can hold 520 bytes (ascii characters)
@@ -106,27 +100,21 @@ var loremIpsumDummyText = "Lorem ipsum dolor sit amet, consectetur adipiscing el
 // ----------------------------------------------------------------------
 /* shareRecord() */
 // ----------------------------------------------------------------------
+
 function shareRecord() {
-	
-	if(typeof(Storage)!=="undefined")
-	{
-		if (sessionStorage.healthRecordJSONtext)
-		{
-			if (qrToShow)
-			{
-				// Do nothing
-			}
-			else
-			{
-				var qrToShow = sessionStorage.healthRecordJSONtext;
-				// var qrToShow = loremIpsumDummyText;
-				$('#result').append(showQRCode(qrToShow));
-			}
-		}
-		else
-		{
-			document.getElementById("result").innerHTML="Sorry, no record is available";
-		}
-	}
-		
+
+  if (typeof(Storage) !== "undefined") {
+    if (sessionStorage.healthRecordJSONtext) {
+      if (qrToShow) {
+        // Do nothing
+      } else {
+        var qrToShow = sessionStorage.healthRecordJSONtext;
+        // var qrToShow = loremIpsumDummyText;
+        $('#result').append(showQRCode(qrToShow));
+      }
+    } else {
+      document.getElementById("result").innerHTML = "Sorry, no record is available";
+    }
+  }
+
 }
